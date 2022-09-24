e638601d37de26bcbdd09ef229a59e2a489db626
Shubham Bairagi <sbairagi825@gmail.com>
2022-09-24 11:44:52 +0530

    change styling

--- src/app/service/todo.service.ts
+++ src/app/service/todo.service.ts
@@ -8,20 +8,20 @@ import { BehaviorSubject } from 'rxjs';
 export class TodoService {
 
   todo: BehaviorSubject<any> = new BehaviorSubject([
-    {'id': 1, 'title': "Get to work", "desc": " test description", "status": 'todo'},
-    {'id': 2, 'title': "Pick up groceries",  "desc": " test description", "status": 'todo'},
-    {'id': 3, 'title': "Fall asleep",  "desc": " test description", "status": 'todo'},
-    {'id': 4, 'title': "Go home",  "desc": " test description", "status": 'todo'},
-    
-
+    {'id': 1, 'title': "Get to work", "desc": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium laborum asperiores", "status": 'todo'},
+    {'id': 2, 'title': "Pick up groceries",  "desc": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium laborum asperiores", "status": 'todo'},
+    {'id': 3, 'title': "Fall asleep",  "desc": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium laborum asperiores", "status": 'todo'},
+    {'id': 4, 'title': "Go home",  "desc": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium laborum asperiores", "status": 'todo'},
   ]);
+
   done: BehaviorSubject<any> = new BehaviorSubject([
-    {'id': 5, 'title': "Brush teeth", "desc": " test description", "status": 'done'},
-    {'id': 6, 'title': "Take a shower", "desc": " test description", "status": 'done'},
-    {'id': 7, 'title': "Check e-mail", "desc": " test description", "status": 'done'},
-    {'id': 8, 'title': "Walk dog", "desc": " test description", "status": 'done'},
-  ])
-  inprogress: BehaviorSubject<any> = new BehaviorSubject([{'id': 9, 'title': "Get up", "desc": " test description", "status": 'inprogress'},])
+    {'id': 5, 'title': "Brush teeth", "desc": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium laborum asperiores", "status": 'done'},
+    {'id': 6, 'title': "Take a shower", "desc": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium laborum asperiores", "status": 'done'},
+    {'id': 7, 'title': "Check e-mail", "desc": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium laborum asperiores", "status": 'done'},
+    {'id': 8, 'title': "Walk dog", "desc": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium laborum asperiores", "status": 'done'},
+  ]);
+
+  inprogress: BehaviorSubject<any> = new BehaviorSubject([{'id': 9, 'title': "Get up", "desc": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium laborum asperiores", "status": 'inprogress'},]);
 
   constructor() {
 
